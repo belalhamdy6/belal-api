fc52a24f4a583beaafdbaa8924d4da756af33846
belalhamdy6 <belalhamde6@gmail.com>
2020-09-15 15:16:46 +0200

    add new

--- speed/models.py
+++ speed/models.py
@@ -8,6 +8,10 @@ class Client(models.Model):
     client_name = models.CharField(max_length=30)
     client_phone = models.IntegerField()
     client_address = models.CharField(max_length=200)
+    latitude = models.CharField(max_length=20)
+    longitude = models.CharField(max_length=20)
+    price_product = models.CharField(max_length=20)
+
 
     def __str__(self):
         return self.client_name
